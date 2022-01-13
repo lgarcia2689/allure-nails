d3677e393f16d62c2b3b77edffe88019ebe74a2b
Luis Garcia <jrluis21@gmail.com>
2022-01-13 14:10:30 -0600

    changes made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom'
+import Nail from './Nail'
+import Services  from "./Services";
+import Gallery from "./Gallery";
+import Events from "./Events";
+import Contact from "./Contact";
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+ 
+      <Router>
+      <nav>
+          <header className="App-header">
+        <div className="App-image">
+          <div className='logo'>
+
+          <a  href="Gallery"><img src="https://i0.wp.com/luxenailboutique.com/wp-content/uploads/2018/07/header-flowers.png?resize=300%2C133" title="insert title" alt="insert description here"/></a>
+
+           
+            
+          </div>
+          <div className='submenu'>
+            <p className ='home'><Link to="/">Home </Link></p>
+            <p className='services'><Link to='Services'>Services</Link></p>
+            <p className='services'><Link to='Gallery'></Link></p>
+            <p className='events'><Link to='Events'>Events</Link></p>
+            <p className='contact'><Link to='Contact'>Contact</Link></p>
+            
+          </div>
+        </div>
       </header>
+
+          {/* Links to navigate us Home (`/`) and Characters (`/Characters`) */}
+ 
+ 
+      </nav>
+        <Switch>
+        <Route path="/Contact">
+            <Contact/>
+          </Route>
+             <Route path="/Events">
+            <Events/>
+          </Route>
+          <Route path="/Gallery">
+            <Gallery/>
+          </Route>
+             <Route path="/Services">
+            <Services/>
+          </Route>
+          <Route path="/">
+            <Nail/>
+          </Route>
+          
+
+        </Switch>
+        </Router>
     </div>
   );
-}
-
+};
 export default App;
